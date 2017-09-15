cdd01d3f51ec3f91359179d4e527c77fa0b0dfcb
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-09-15 11:50:30 +0300

    Phonebook

--- src/Exercises/Arrays/More_Exercises/P03_Phonebook.java
+++ src/Exercises/Arrays/More_Exercises/P03_Phonebook.java
@@ -1,5 +1,25 @@
 package Exercises.Arrays.More_Exercises;
 
+import java.util.Scanner;
+
 public class P03_Phonebook {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] phoneBook = scanner.nextLine().split(" ");
+        String[] names = scanner.nextLine().split(" ");
+
+        while (true) {
+            String currentName = scanner.nextLine();
+            if (currentName.equals("done")) {
+                break;
+            }
+            for (int i = 0; i < names.length; i++) {
+                if (names[i].equals(currentName)) {
+                    System.out.println(names[i] + " -> " + phoneBook[i]);
+                    break;
+                }
+            }
+        }
+    }
 }
